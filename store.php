9786499f78d8887e8af56a4808f9c8134da6c714
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-07-29 23:25:57 +0530

    writing form submission logic

--- store.php
+++ store.php
@@ -3,6 +3,18 @@
     <link rel="stylesheet" href="./loginPage.css">
     </head>
     <body>
+    <div class="page__body">
+        <?php
+        $name=$_POST['t1'];
+        $pass=$_POST['t2'];
+        $email=$_POST['t3'];
+        $uid=date('dNYBHis');
+        $file = fopen("details.txt", 'a');
+        $text = $name."::".$pass."::".$email."::".$uid."\r\n";
+        fwrite($file, $text);
+        fclose($file);
+     
+    ?>
         
     </body>
 
