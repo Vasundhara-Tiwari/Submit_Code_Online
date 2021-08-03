7b7d8b15a0a8e0ae2b1e3286bffbdc431f753d4f
Vasundhara Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-08-03 23:36:10 +0530

    Adding validation logic

---
+++ validate.php
@@ -0,0 +1,31 @@
+<?php
+$email=$_POST['email'];
+$psw=$_POST['password'];
+
+$flag=0;
+$s=array();
+$file = fopen("details.txt", 'r');
+while(!feof($file)) {
+
+  $s=fgets($file);
+  $str_arr = array_pad(explode('::', $s),3,null);
+  
+  if ($str_arr[1]==$psw and $str_arr[2]==$email){
+  
+    session_start();
+    $_SESSION['email']=$email;
+    $_SESSION['last_login'] = time();
+  
+    header("refresh: 0; url=./Evaluation.php");
+	
+    echo "Authorized User";
+    
+	
+    $flag=1;
+	  	break;
+	}
+}//end while
+if ($flag==0)
+	echo "Please register before login";
+fclose($file);
+?>
